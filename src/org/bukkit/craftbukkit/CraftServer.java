ddcd163628bc23c3b5577c575f8425e455dcd1a8
Owen2k6 <fulptdm@gmail.com>
2023-02-18 20:25:29 +0000

    PoseidonPlus release ident
    Soon to be released dw

--- src/org/bukkit/craftbukkit/CraftServer.java
+++ src/org/bukkit/craftbukkit/CraftServer.java
@@ -55,8 +55,8 @@ public final class CraftServer implements Server {
     private final String serverName = "Poseidon Plus Craftbukkit";
     //Poseidon Versions
     private final String serverEnvironment = "POSEIDON";
-    private final String serverVersion = "1.1.7+";
-    private final String releaseType = "DEVELOPMENT";
+    private final String serverVersion = "1.0";
+    private final String releaseType = "RELEASE";
     private final String protocolVersion = "1.7.3";
     private final String GameVersion = "b1.7.3";
     private final ServicesManager servicesManager = new SimpleServicesManager();
